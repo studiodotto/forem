582241aa242e89cfeb29430d5ef1007d27172971
Muzzamil123 <muzamil.mujahid11@gmail.com>
2021-01-07 19:43:54 +0500

    unique check added

--- app/models/artist_application.rb
+++ app/models/artist_application.rb
@@ -1,4 +1,7 @@
 class ArtistApplication < ApplicationRecord
   has_many :services
   belongs_to :user, optional: true
+  
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
