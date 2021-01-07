04f16533dcad596f3d38a9348e75801f2d79e0c0
Muzzamil123 <muzamil.mujahid11@gmail.com>
2021-01-07 20:12:28 +0500

    improvements added

--- app/models/artist_application.rb
+++ app/models/artist_application.rb
@@ -1,7 +1,6 @@
 class ArtistApplication < ApplicationRecord
-  has_many :services
+  has_many :services, dependent: :destroy
   belongs_to :user, optional: true
-
   validates :email, uniqueness: true
   validates :username, uniqueness: true
 end
