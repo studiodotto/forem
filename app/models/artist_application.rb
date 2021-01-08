f03869edf4fcbaa859ff0f164e7c7ae9a45f0ca1
Muzzamil123 <muzamil.mujahid11@gmail.com>
2021-01-08 16:57:21 +0500

    Banner updated

--- app/models/artist_application.rb
+++ app/models/artist_application.rb
@@ -2,5 +2,4 @@ class ArtistApplication < ApplicationRecord
   has_many :services, dependent: :destroy
   belongs_to :user, optional: true
   validates :email, uniqueness: true
-  validates :username, uniqueness: true
 end
