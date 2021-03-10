9a917ea85c90fe461e40049fd75a8689bf84ca2c
Muzzamil123 <muzamil.mujahid11@gmail.com>
2021-03-10 17:30:02 +0500

    details added

--- app/assets/javascripts/initializers/initializeTouchDevice.js
+++ app/assets/javascripts/initializers/initializeTouchDevice.js
@@ -25,6 +25,7 @@ function removeShowingMenu() {
 
 function toggleMenu() {
   getClassList('crayons-header__menu').toggle('showing');
+  getClassList('crayons-header__menuu').toggle('showing');
 }
 
 function initializeTouchDevice() {
@@ -39,6 +40,7 @@ function initializeTouchDevice() {
     if (isTouchDevice) {
       // Use a named function instead of anonymous so duplicate event handlers are discarded
       getById('navigation-butt').addEventListener('click', toggleMenu);
+      getById('navigation-buttt').addEventListener('click', toggleMenu);
     } else {
       getClassList('crayons-header__menu').add('desktop');
       getById('navigation-butt').addEventListener('focus', (e) =>
@@ -48,6 +50,15 @@ function initializeTouchDevice() {
         blur(e, 'second-last-nav-link'),
       );
       getById('navigation-butt').addEventListener('blur', (e) =>
+        blur(e, 'first-nav-link'),
+      );
+      getById('navigation-buttt').addEventListener('focus', (e) =>
+        getClassList('crayons-header__menu').add('showing'),
+      );
+      getById('last-nav-link').addEventListener('blur', (e) =>
+        blur(e, 'second-last-nav-link'),
+      );
+      getById('navigation-buttt').addEventListener('blur', (e) =>
         blur(e, 'first-nav-link'),
       );
     }
